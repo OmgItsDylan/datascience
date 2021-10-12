b41d3d854c96509f1c272582c4480e3a6dd98348
OmgItsDylan <62649728+OmgItsDylan@users.noreply.github.com>
2021-10-12 12:19:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def main():
 	input = np.array(features).reshape(1,-1)
 	
 	if st.button("FOO"):
-		model = pickle.load(open("model.pkl", 'rb'))
+		model = pickle.load(open("model2.pkl", 'rb'))
 		prediction = model.predict(input)
 		st.write(f"{prediction.item().title()} are recommended by the A.I for your farm.")
 

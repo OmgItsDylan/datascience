c4dc969c5d2ec1c649f05bb920a397de0741f54e
OmgItsDylan <62649728+OmgItsDylan@users.noreply.github.com>
2021-10-12 11:51:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,21 +7,7 @@ import warnings
 
 
 def main():
-	st.markdown(html_temp, unsafe_allow_html=True)
-	with col2:
-		st.subheader(" Find out the most suitable crop to grow in your farm 👨‍🌾")
-		N = st.number_input("Nitrogen", 1,10000)
-		P = st.number_input("Phosporus", 1,10000)
-		K = st.number_input("Potassium", 1,10000)
-		temp = st.number_input("Temperature",0.0,100000.0)
-		humidity = st.number_input("Humidity in %", 0.0,100000.0)
-		ph = st.number_input("Ph", 0.0,100000.0)
-		rainfall = st.number_input("Rainfall in mm",0.0,100000.0)
-		feature_list = [N, P, K, temp, humidity, ph, rainfall]
-		single_pred = np.array(feature_list).reshape(1,-1)
-		if st.button('Predict'):
-			loaded_model = load_model('model.pkl')
-			prediction = loaded_model.predict(single_pred)
+	st.title('Crop Prediction')
 
 if __name__ == '__main__':
 	main()

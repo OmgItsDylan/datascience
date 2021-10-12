a9d230c757b3e993b48ed18b76affdb6fb50f353
OmgItsDylan <62649728+OmgItsDylan@users.noreply.github.com>
2021-10-12 12:04:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,14 @@ import warnings
 def main():
 	st.title('Crop Prediction')
 	n = st.number_input("nitrogen", 1,10000)
-  p = st.number_input("phosporus", 1,10000)
-  k = st.number_input("potassium", 1,10000)
-  temp = st.number_input("temperature",0.0,100000.0)
-  humid = st.number_input("humidity in %", 0.0,100000.0)
-  ph = st.number_input("ph", 0.0,100000.0)
-  rain = st.number_input("rainfall in mm",0.0,100000.0)
-
-  features = [n, p, k, temp, humid, ph, rain]
-  input = np.array(features).reshape(1,-1)
+	p = st.number_input("phosporus", 1,10000)
+	k = st.number_input("potassium", 1,10000)
+	temp = st.number_input("temperature",0.0,100000.0)
+	humid = st.number_input("humidity in %", 0.0,100000.0)
+	ph = st.number_input("ph", 0.0,100000.0)
+	rain = st.number_input("rainfall in mm",0.0,100000.0)
+	features = [n, p, k, temp, humid, ph, rain]
+	input = np.array(features).reshape(1,-1)
 	
 	if st.button("FOO"):
 		model = pickle.load(open("model.plk", 'rb'))
